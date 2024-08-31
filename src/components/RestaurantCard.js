d17dc464df613353d64a97dd79ae2207eb833353
Yasir <yasir.hasan@outlook.com>
2024-09-01 00:07:31 +0500

    UseState

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -2,7 +2,7 @@ const cardStyle = { marginTop: 5, marginBottom: 5 };
 
 const RestaurantCard = (props) => {
   const { restData } = props;
-  const { name, image, servesCuisine, address, telephone } = restData?.item;
+  const { name, image, servesCuisine, address, avgRating } = restData?.item;
 
   return (
     <div className="rest-card" style={{ backgroundColor: "#f0f0f0" }}>
@@ -10,7 +10,7 @@ const RestaurantCard = (props) => {
       <h3 style={cardStyle}>{name}</h3>
       <h5 style={cardStyle}>{servesCuisine.join(", ")}</h5>
       <h5 style={cardStyle}>{address.streetAddress}</h5>
-      <h5 style={cardStyle}>{"Ph: " + telephone}</h5>
+      <h5 style={cardStyle}>{avgRating}</h5>
     </div>
   );
 };
